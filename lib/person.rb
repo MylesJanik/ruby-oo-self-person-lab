20392ce5326c0435bd33ee3bcae0bfd28dac4919
Myles <mylesjanik@gmail.com>
2020-11-15 17:37:30 -0800

    commit tree ddbe7a0c4df09c967967840899e9fc71642d2a55

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,89 @@
 # your code goes here
+require 'pry'
+class Person
+    attr_reader :name
+    attr_accessor :bank_account
+    attr_reader :happiness
+    attr_reader :hygiene
+
+    def initialize(name)
+        @name = name
+        @happiness = 8
+        @hygiene = 8
+        @bank_account = 25
+    end
+
+    def happiness=(num)
+        if num > 10
+            @happiness = 10
+        elsif num < 0
+            @happiness = 0
+        else
+            @happiness = num
+        end
+    end
+
+    def hygiene=(num)
+        if num > 10
+            @hygiene = 10
+        elsif num < 0
+            @hygiene = 0
+        else
+            @hygiene = num
+        end
+    end
+
+    def happy?
+        if @happiness > 7
+            true
+        else
+            false
+        end
+    end
+
+    def clean?
+        if @hygiene > 7
+            true
+        else
+            false
+        end
+    end
+
+    def get_paid(salary)
+        @bank_account = @bank_account + salary
+        return "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene = (@hygiene + 4)
+        return "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.happiness = (@happiness + 2)
+        self.hygiene = (@hygiene - 3)
+        return "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        friend.happiness = friend.happiness + 3
+        self.happiness = (@happiness + 3)
+
+        return "Hi #{friend.name}! It's #{@name}. How are you?"
+    end
+
+    def start_conversation(friend, topic)
+        if topic == "politics"
+            friend.happiness = friend.happiness - 2
+            self.happiness = (@happiness - 2)
+            return "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            friend.happiness = friend.happiness + 1
+            self.happiness = (@happiness + 1)
+            return "blah blah sun blah rain"
+        else
+            return "blah blah blah blah blah"
+        end
+    end
+
+end
